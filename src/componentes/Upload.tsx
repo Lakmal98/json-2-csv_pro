c558cf98e9f21f13c9b3c0b7fe16f53416818f6c
Dimuthu Lakmal <lakmalepp@gmail.com>
2021-09-16 10:26:45 +0530

    Add download functionality

--- src/componentes/Upload.tsx
+++ src/componentes/Upload.tsx
@@ -1,18 +1,32 @@
 import {
+  Button,
   FormControl,
   FormHelperText,
   TextareaAutosize,
 } from "@material-ui/core";
 import React, { Component } from "react";
+import { CsvToHtmlTable } from "react-csv-to-table";
 
 interface Props {}
-interface State {}
+interface State {
+  json: Object;
+  prettify: any;
+  csv: any;
+}
+
+const LIMIT = 500;
 
 export default class Upload extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      json: {},
+      prettify: "",
+      csv: "",
+    };
+
+    this.download = this.download.bind(this);
   }
 
   handleChange = (event: any) => {
@@ -24,17 +38,42 @@ export default class Upload extends Component<Props, State> {
   };
 
   convert(json: any) {
-    let converter = require("json-2-csv");
+    const converter = require("json-2-csv");
 
     const callbackFunction = (err: any, csv: any) => {
       if (err) console.log("An Error");
-      else console.log("Csv", csv);
+      else {
+        this.setState({ json, csv });
+      }
     };
 
     converter.json2csv(json, callbackFunction);
   }
 
+  download(): void {
+    const { csv } = this.state;
+    //download the csv
+    const blob = new Blob([csv], {
+      type: "text/csv;charset=utf-8;",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${new Date().toString()}.csv`;
+    a.click();
+  }
+
   render(): JSX.Element {
+    let { csv } = this.state;
+
+    //if limit is exceeded, get the first 500 lines
+    if (csv.length > LIMIT) {
+      csv = csv
+        .split("\n")
+        .slice(0, LIMIT + 1)
+        .join("\n");
+    }
+
     return (
       <>
         <FormControl>
@@ -50,6 +89,12 @@ export default class Upload extends Component<Props, State> {
             Enter an array of objects in JSON format
           </FormHelperText>
         </FormControl>
+        <Button color="primary" variant="contained" onClick={this.download}>
+          Download
+        </Button>
+        <div className="csv">
+          <CsvToHtmlTable data={csv} csvDelimiter="," />
+        </div>
       </>
     );
   }
